0584732294a337abc9e55b18ffa8a1dbda4b6a6e
p-lyp <felipealves13tga@hotmail.com>
2023-07-30 12:43:01 -0400

    correcao2-cors

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express();
 
 app.use(express.json());
 
-const storedData = [];
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
-server.use(
-    cors({
-        origin: "*",
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    })
-);
+const storedData = [];
 
 app.get("/", (req, res) => {
     console.log("Just got a request!");
