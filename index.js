41dfd53365aed20e19309e209bac6a3d35ed7bd8
p-lyp <felipealves13tga@hotmail.com>
2023-08-01 19:51:43 -0400

    teste-s3

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
 
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
 app.use(express.json());
+
+// store something
+await s3
+    .putObject({
+        Body: JSON.stringify({ key: "value" }),
+        Bucket: "cyclic-misty-boa-garment-sa-east-1",
+        Key: "some_files/my_file.json",
+    })
+    .promise();
+
+// get it back
+let my_file = await s3
+    .getObject({
+        Bucket: "cyclic-misty-boa-garment-sa-east-1",
+        Key: "some_files/my_file.json",
+    })
+    .promise();
+
+console.log(JSON.parse(my_file));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); // update to match the domain you will make the request from
